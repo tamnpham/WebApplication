68584c7095cbdf84bb84c98493eb4180c27999f1
Huỳnh Minh Trí <53149836+trivonhan@users.noreply.github.com>
2021-11-12 11:28:11 +0700

    New Quiz page

---
+++ frontend/dashboard/src/pages/Quiz.js
@@ -0,0 +1,23 @@
+import { Icon } from '@iconify/react';
+import plusFill from '@iconify/icons-eva/plus-fill';
+import { Link as RouterLink } from 'react-router-dom';
+// material
+import { Button, Container, Stack, Typography } from '@mui/material';
+// components
+import Page from '../components/Page';
+import ActionAreaCard from '../components/blog/PostCard'
+//
+
+// ----------------------------------------------------------------------
+
+// ----------------------------------------------------------------------
+
+export default function Quiz() {
+  return (
+    <Page title="Quiz">
+      <Container>
+      
+      </Container>
+    </Page>
+  );
+}
