71c13a0d72d35c474a54ad2d2ae3717c02943447
tamnpham <phamngoctam2405it@gmail.com>
2021-11-29 11:35:05 +0700

    add loading page

--- frontend/dashboard/src/pages/Post.js
+++ frontend/dashboard/src/pages/Post.js
@@ -1,58 +1,16 @@
 // material
 import { Container, Typography, Grid, Paper,Box } from "@mui/material";
 import { styled } from '@mui/material/styles';
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from 'react-markdown';
+import LinearProgress from '@mui/material/LinearProgress';
 // components
 import Page from '../components/Page';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
     useParams
   } from "react-router-dom";
 import { useEffect, useState } from 'react';
 
 // ----------------------------------------------------------------------
-
-// function createData(id, imgLink, by, time, title, content) {
-//     return { id, imgLink, by, time, title, content };
-// }
-  
-// const posts = [
-//   createData(
-//     "0",
-//     "https://media.istockphoto.com/photos/top-view-of-a-blue-desktop-with-copy-space-picture-id1278436436?b=1&k=20&m=1278436436&s=170667a&w=0&h=QrFR5_k1AKgs-1ypGE0QhqHQlqZzBYbtVafn7rD8sUQ=",
-//     "tamnpham",
-//     "27/11/2021",
-//     "Giới thiệu LSExam",
-//     `<h1>What is Lorem Ipsum?</h1>
-//         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        
-//         Why do we use?
-//         It is a long established fact that a reader will be distracted by the readable content of a when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-        
-//         Where does come from?
-//         Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, comes from a line in section 1.10.32.
-        
-//         The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`
-//   ),
-//   createData(
-//     "1",
-//     "https://www.wpbeginner.com/wp-content/uploads/2016/11/blogimagetools.jpg",
-//     "trivonhan",
-//     "24/11/2021",
-//     "Giới thiệu LSExam",
-//     "Trang  giới thiệu về LSExxam"
-//   ),
-//   createData(
-//     "2",
-//     "https://ngocdenroi.com/wp-content/uploads/2021/01/cach-quang-ba-blog-tang-traffic-hieu-qua.jpg",
-//     "tamnpham",
-//     "27/11/2021",
-//     "Deep Learning with Javascript",
-//     "Trang  giới thiệu về LSExxam"
-//   ),
-// ];
 
 const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
@@ -62,6 +20,7 @@ const Item = styled(Paper)(({ theme }) => ({
   }));
 
 
+// const rootStyle = styled(Body)
 // ----------------------------------------------------------------------
 
 export default function Post() {
@@ -94,18 +53,16 @@ export default function Post() {
             // fetchPostData()
           },[])
       
-          console.log(postState);
-
-    console.log();
     if (postState != null) {
     return (
+      // <div sx={{width: '100%', height: '100%', backgroundColor: '#161d31'}}>
       <Page
         title="Post Page"
         sx={{
           p: "5%",
           backgroundColor: "#161d31",
           color: "white",
-          // height: "100%",
+          height: "100%",
         }}
       >
         <Container>
@@ -141,8 +98,33 @@ export default function Post() {
           </Grid>
         </Container>
       </Page>
+      // </div>
     );
   } else {
-    return <Typography variant="h2">Loading...</Typography>;
+    return (
+      <>
+        <Page
+          sx={{
+            p: "5%",
+            backgroundColor: "#161d31",
+            color: "white",
+            height: "100%",
+          }}
+        >
+          <div
+            style={{
+              position: "absolute",
+              left: "50%",
+              top: "50%",
+              width: '80%',
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            <Typography variant="h1" color='white' textAlign='center'> Loading... </Typography>
+            <LinearProgress color="success" />
+          </div>
+        </Page>
+      </>
+    );
   }
 }
