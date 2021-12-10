12268fe05c924029f11b6621f489c64ff5b1da4e
Long Nguyen <hoanglong1622000@gmail.com>
2021-12-10 14:13:58 +0700

    Fixed url error in migrating to other VM

--- backend/apps/questions/serializers.py
+++ backend/apps/questions/serializers.py
@@ -19,7 +19,11 @@ class QuestionSerializer(serializers.ModelSerializer):
 
         if instance.image and instance.image.url:
             image_url = instance.image.url
-            image_url = request.build_absolute_uri(image_url)
+            # image_url = request.build_absolute_uri(image_url)
+            full_domain = 'http://' + request.META['HTTP_HOST']
+            if request.META["SERVER_PORT"] not in full_domain:
+                full_domain += ':' + request.META["SERVER_PORT"]
+            image_url = full_domain + image_url
         return image_url
 
 
