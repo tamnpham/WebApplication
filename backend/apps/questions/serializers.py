a9188e171c9276dde3b75e2423aadba1f426b358
Long Nguyen <hoanglong1622000@gmail.com>
2021-12-13 00:32:02 +0700

    Minor change to Question serializer

--- backend/apps/questions/serializers.py
+++ backend/apps/questions/serializers.py
@@ -1,25 +1,9 @@
 from django.conf import settings
 from rest_framework import serializers
 
-from .models import Category, Question
 from apps.core.serializers import CustomSerializerMixin
 
-
-class QuestionSerializer(
-    serializers.ModelSerializer,
-    CustomSerializerMixin,
-):
-    """Serializer for representing `Question`."""
-    image_url = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Question
-        fields = "__all__"
-
-    # https://stackoverflow.com/a/35522896
-    def get_image_url(self, instance):
-        """Customize image serialization method."""
-        return self.to_url(instance.image)
+from .models import Category, Question
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -32,3 +16,29 @@ class CategorySerializer(serializers.ModelSerializer):
             "created",
             "modified",
         )
+
+
+class QuestionSerializer(
+    serializers.ModelSerializer,
+    CustomSerializerMixin,
+):
+    """Serializer for representing `Question`."""
+    categoryInfo = serializers.SerializerMethodField(read_only=True)
+    image_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Question
+        fields = "__all__"
+
+    # https://stackoverflow.com/a/35522896
+    def get_image_url(self, instance):
+        """Customize image serialization method."""
+        return self.to_url(instance.image)
+
+    def get_categoryInfo(self, instance):
+        """Get serialized category information."""
+        category = instance.category
+        return {
+            "id": category.id,
+            "name": category.name
+        }
